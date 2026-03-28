82625e62e09894f5221da9091ea6e1a0aa1f7c48
Carlos Eduardo <carloscosta8001@gmail.com>
2026-03-28 18:03:45 -0300

    feat:Adicionando atributo de transcrição e chave estrangeira no Desafio

--- desafIAr/models.py
+++ desafIAr/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Video(models.Model):
     titulo = models.CharField(max_length=100)
-    descricao = models.TextField()
+    transcricao = models.TextField()
     url = models.URLField()
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
@@ -13,6 +13,7 @@ class Video(models.Model):
 
 
 class Desafio(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='desafios', null=True, blank=True)
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     data_criacao = models.DateTimeField(auto_now_add=True)
